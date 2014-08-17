44a54a4f0b86bde015aed925072e2d1475a35178
kate.wu <uncertain29161@gmail.com>
2014-08-17 21:44:09 +0800

    new add

--- create.php
+++ create.php
@@ -6,30 +6,34 @@ $post_data = $_POST;
 
 switch ($post_data['insert_type']) {
 	case 'member':
+		$name = addslashes($post_data['name']);
+		$email = addslashes($post_data['email']);
 		// 判斷是否已存在
-		$chk_sql = "SELECT * FROM users WHERE email='{$post_data['email']}'";
+		$chk_sql = "SELECT * FROM users WHERE email='{$email}'";
 		$res = mysqli_query($con,$chk_sql);
 		$row = mysqli_fetch_assoc($res);
 		if(!$row){
 			// 不存在 -> 新增
-			$sql = "INSERT INTO users (name, sex, email) VALUES ('{$post_data['name']}', '{$post_data['sex']}','{$post_data['email']}')";
+			$sql = "INSERT INTO users (name, sex, email) VALUES ('{$name}', '{$post_data['sex']}','{$email}')";
 			mysqli_query($con,$sql);
 		}
 		break;
 
 	case 'category':
+		$name = addslashes($post_data['name']);
 		// 判斷是否已存在
-		$chk_sql = "SELECT * FROM categories WHERE name='{$post_data['name']}'";
+		$chk_sql = "SELECT * FROM categories WHERE name='{$name}'";
 		$res = mysqli_query($con,$chk_sql);
 		$row = mysqli_fetch_assoc($res);
 		if(!$row){
 			// 不存在 -> 新增
-			$sql = "INSERT INTO categories (name) VALUES ('{$post_data['name']}')";
+			$sql = "INSERT INTO categories (name) VALUES ('{$name}')";
 			mysqli_query($con,$sql);
 		}
 		break;
 
 	case 'singer':
+		$name = addslashes($post_data['name']);
 		// 判斷是否已存在
 		$chk_sql = "SELECT * FROM singers WHERE name='{$post_data['name']}'";
 		$res = mysqli_query($con,$chk_sql);
@@ -38,12 +42,14 @@ switch ($post_data['insert_type']) {
 			$type_array = array_merge($post_data['singers'],$post_data['location']);
 			$singer_type = implode(',', $type_array);
 			// 不存在 -> 新增
-			$sql = "INSERT INTO singers (name,type) VALUES ('{$post_data['name']}','{$singer_type}')";
+			$sql = "INSERT INTO singers (name,type) VALUES ('{$name}','{$singer_type}')";
 			mysqli_query($con,$sql);
 		}
 		break;
 
 	case 'song':
+		$name = addslashes($post_data['name']);
+		$lyrics = addslashes($post_data['lyrics']);
 		// 判斷是否已存在
 		$chk_sql = "SELECT * FROM songs WHERE name='{$post_data['name']}'";
 		$res = mysqli_query($con,$chk_sql);
@@ -51,7 +57,7 @@ switch ($post_data['insert_type']) {
 
 		if(!$row){
 			// 不存在 -> 新增
-			$sql = "INSERT INTO songs (name,lyrics) VALUES ('{$post_data['name']}','{$post_data['lyrics']}')";
+			$sql = "INSERT INTO songs (name,lyrics) VALUES ('{$name}','{$lyrics}')";
 			$result = mysqli_query($con,$sql);
 
 			$new_res = "SELECT id FROM songs ORDER BY id desc LIMIT 1";
@@ -81,8 +87,9 @@ switch ($post_data['insert_type']) {
 		break;
 
 	case 'album':
+		$name = addslashes($post_data['name']);
 		// 判斷是否已存在
-		$chk_sql = "SELECT * FROM album WHERE name='{$post_data['name']}'";
+		$chk_sql = "SELECT * FROM album WHERE name='{$name}'";
 		$res = mysqli_query($con,$chk_sql);
 		$row = mysqli_fetch_assoc($res);
 
@@ -90,7 +97,7 @@ switch ($post_data['insert_type']) {
 			// 不存在 -> 新增
 			$singer_info = implode(',', $post_data['singer']);
 			$song_info = implode(',', $post_data['song']);
-			$sql = "INSERT INTO albums (name,cate_id,singer_info,song_info) VALUES ('{$post_data['name']}','{$post_data['cate_id']}','{$singer_info}','{$song_info}')";
+			$sql = "INSERT INTO albums (name,cate_id,singer_info,song_info) VALUES ('{$name}','{$post_data['cate_id']}','{$singer_info}','{$song_info}')";
 			mysqli_query($con,$sql);
 
 			$new_res = "SELECT id FROM albums ORDER BY id desc LIMIT 1";
@@ -128,19 +135,21 @@ switch ($post_data['insert_type']) {
 		break;
 
 	case 'playlist':
+		$name = addslashes($post_data['name']);
 		// 判斷是否已存在
-		$chk_sql = "SELECT * FROM playlists WHERE name='{$post_data['name']}' AND u_id={$post_data['u_id']}";
+		$chk_sql = "SELECT * FROM playlists WHERE name='{$name}' AND u_id={$post_data['u_id']}";
 		$res = mysqli_query($con,$chk_sql);
 		$row = mysqli_fetch_assoc($res);
 		if(!$row){
 			$song_info = implode(',', $post_data['song']);
 			// 不存在 -> 新增
-			$sql = "INSERT INTO playlists (u_id,name,song_info) VALUES ({$post_data['u_id']},'{$post_data['name']}','{$song_info}')";
+			$sql = "INSERT INTO playlists (u_id,name,song_info) VALUES ({$post_data['u_id']},'{$name}','{$song_info}')";
 			mysqli_query($con,$sql);
 		}
 		break;
 	default:
-		# code...
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: form.php");
 		break;
 }
 header("HTTP/1.1 301 Moved Permanently");
